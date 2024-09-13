809a1fa4336bc153b4058aad9aeacb120054638e
N1kolozPetriashvili <NikolozPetriashvili2010@gmail.com>
2024-09-13 19:29:47 +0400

    third commit

---
+++ Level 03/ClassWork.py
@@ -0,0 +1,14 @@
+name = 'ნიკოლოზი'
+last_name = 'პეტრიაშვილი'
+
+print(name + ' ' + last_name)
+
+print('------------------')
+
+nm1 = 150
+nm2 = 0.15
+
+print(nm1 * nm2)
+print(nm1 + nm2)
+print(nm1 - nm2)
+print(nm1 % nm2)
