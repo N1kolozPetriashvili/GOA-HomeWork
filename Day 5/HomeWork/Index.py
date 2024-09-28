808d4067d539d4c1dff43d567990afaf9492f735
N1kolozPetriashvili <NikolozPetriashvili2010@gmail.com>
2024-09-28 15:25:23 +0400

    ninth commit

---
+++ Day 5/HomeWork/Index.py
@@ -0,0 +1,41 @@
+birth_year = int(input("დაბადების წელი:"))
+current_year = 2024
+
+print('ტქვენ ხართ ' + current_year - birth_year)
+
+print("-------------------------")
+
+length = input("სიგრძე: ")
+width = input("სიგანე: ")
+
+print(int(length) * int(width))
+print(2 * (int(length) + int(width)))
+
+print("-------------------------")
+
+distance = input("მანძილი: ")
+
+print(int(distance) * 1000)
+print(int(distance) * 100000)
+print(int(distance) * 1000000) 
+
+print("-------------------------")
+
+name = input("სახელი: ")
+surname = input("გვარი: ")
+name1 = input("მშობლის სახელი: ")
+surname1 = input("მშობლის გვარი: ")
+name2 = input("მშობლის სახელი: ")
+surname2 = input("მშობლის გვარი: ")
+hobby = input("ჰობი: ") 
+hobby1 = input("ჰობი: ")
+hobby2 = input("ჰობი: ")
+
+print("მე მქვია " + name + " " + surname + ' ' + "და ჩემი მშობლები არიან " + name1 + " " + surname1 + ' ' + 'და' + name2 + " " + surname2 + ' ' + ". ჩემი ჰობები არის " + hobby + " "+ hobby1 + " "+ hobby2)
+
+print("-------------------------")
+
+num1 = int(input('რიცხვი: '))
+
+print(num1 //)
+print(num1 %)
