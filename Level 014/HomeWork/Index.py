75a3d13bdddd02b505c7c63b0633e729663b1c70
N1kolozPetriashvili <NikolozPetriashvili2010@gmail.com>
2024-10-31 21:55:51 +0400

    eighteenth commit

--- Level 014/HomeWork/Index.py
+++ Level 014/HomeWork/Index.py
@@ -14,7 +14,12 @@ for i in range(num):
 
 print("----------")
 
-
+result = 0
+for i in range(5):
+     number = int(input("გთხოვთ შემოიტანოთ ციფრი: "))
+     result += number
+average = result / 5
+print(average)
 
 print("----------")
 
@@ -24,6 +29,15 @@ for i in range(-100, 100):
 
 print("----------")
 
-
+num = int(input("შემოიტანეთ ციფრი: "))
+number = 0
+while num > number:
+     num = int(input("შემოიტანეთ ციფრი: "))
+print(" ")
 
 print("----------")
+
+rows = 8
+for i in range(1, rows + 1):
+    print(" " * (rows - i) + "*" * i, end = "")
+    print(" " * (rows + i) + "*" * i)
